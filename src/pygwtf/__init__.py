9142ba5ff882b29c01411532857a3b8584dae889
Christian Chapman-Bird <c.chapmanbird@gmail.com>
2026-06-03 17:18:19 +0100

    first docs setup

---
+++ src/pygwtf/__init__.py
@@ -0,0 +1,8 @@
+"""
+pygwtf: A Python package for time-frequency analysis of gravitational wave data.
+"""
+# from pygwtf import AnalyticTimeFrequencyWaveform
+
+from importlib.metadata import version
+
+__version__ = version("pygwtf")
